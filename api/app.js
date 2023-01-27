4b537abc6f92b06626fa0a4ca8926d429be5b696
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-01-27 17:32:52 +0530

    added mongoDB to the api

--- api/app.js
+++ api/app.js
@@ -1,3 +1,7 @@
+const mongoUSER = "bartyslr"; const mongoPass = "";
+const mongoURI = `mongodb+srv://${mongoUSER}:${mongoPass}@cluster69.7tz3qnk.mongodb.net/?retryWrites=true&w=majority`
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -17,12 +21,12 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.send("pakaya");
+  res.send("pky");
   next();
 });
 
 app.get("/tshirt", (req, res) => {
-  res.status(200).send({ name: "pakaya" });
+  res.status(200).send({ name: "pky" });
 });
 
 app.listen(PORT, () => {
