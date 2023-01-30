306c70110f4ab3ca64bf0a8e763d2676a9f797fe
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-01-31 00:01:01 +0530

    added graphQL

---
+++ api/graph.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+const PORT = "80";
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log("Server fired up!");
+});
