19217697df453f7bfb205af0ada327d7b6a4e8ef
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-02-01 23:18:52 +0530

    added some stuff

--- api/graph.js
+++ api/graph.js
@@ -2,23 +2,41 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
+const { buildSchema, GraphQLScalarType } = require("graphql");
+
+
 
 const schema = buildSchema(`
+  
+  type BlogPost {
+    num: String
+    ins: [Int]
+  }
+  
   type Query {
-    hello: String
+    getMe: BlogPost
+  }
+   
+
+  type Update {
+    updateData(name: String): String
+  }
+
+  schema {
+    query: Query
+    mutation: Update 
   }
 `);
 
 const root = {
-  hello: () => {
-    return "Hello world!";
+  getMe()  {
+    return { num: "[1,2,3,4,5,6,]", ins:[5,4,6]};
   },
 };
 
 const app = express();
 app.use(
-  "/graphql",
+  "/graph",
   graphqlHTTP({
     schema: schema,
     rootValue: root,
