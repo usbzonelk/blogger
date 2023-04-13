3772e8894d9edae05e20d227913846a854b6d63d
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-04-13 12:31:25 +0530

    Added Post.js

---
+++ frontend/Components/Post.js
@@ -0,0 +1,58 @@
+import { Box, Heading, Image, Flex, Text } from "@chakra-ui/react";
+import { useBreakpointValue } from "@chakra-ui/react";
+
+const Post = (props) => {
+  const post = props.post;
+  const author = props.author;
+  const isSmallScreen = useBreakpointValue({ base: true, md: false });
+
+  return (
+    <div style={{ paddingTop: "4rem" }}>
+      <Box
+        w="100%"
+        maxW="800px"
+        marginX="auto"
+        paddingY="8"
+        paddingX={{ base: "4", md: "8" }}
+      >
+        <Heading as="h1" size="2xl" textAlign="center" mb="6">
+          {post.title}{" "}
+        </Heading>
+        <Flex direction={{ base: "column", md: "row" }} align="center" mb="8">
+          <Image
+            src="/images/author.jpg"
+            alt="author"
+            borderRadius="full"
+            boxSize={isSmallScreen ? "80px" : "120px"}
+            objectFit="cover"
+            mr={{ base: "0", md: "4" }}
+            mb={{ base: "4", md: "0" }}
+          />
+          <Box>
+            <Text fontSize="lg" fontWeight="bold">
+              By {author.displayName}
+            </Text>
+            <Text fontSize="md" color="gray.500">
+              {post.fullDate}
+            </Text>
+          </Box>
+        </Flex>
+        <Box>
+          <Image
+            src={post.images.header}
+            alt="post image"
+            w="100%"
+            h={{ base: "auto", md: "500px" }}
+            objectFit="cover"
+            mb="8"
+          />
+          <Text fontSize="xl" mb="6">
+            <div style={{ whiteSpace: "pre-line" }}> {post.content}</div>
+          </Text>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Post;
---
+++ frontend/pages/[slug].js
@@ -0,0 +1,45 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import Post from "../components/Post";
+const PostPage = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const author = {
+    username: "Jon",
+    displayName: "John gaggs",
+  };
+
+  const post = {
+    title: "A Small River by Their Place",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Satis est ad hoc responsum. Quamquam wordpress blog theme recte et reiecta dicere licebit. Quam nemo umquam voluptatem appellavit, appellat erat enim polemonis duo reges constructio. interrete. Nihil opus est exemplis hoc facere longius.",
+    images: {
+      header:
+        "https://a0.muscache.com/im/pictures/e83e702f-ef49-40fb-8fa0-6512d7e26e9b.jpg?aki_policy=large",
+      imgs: [
+        "https://3.bp.blogspot.com/-C0ixAfWi-I4/W-5uxBSTtzI/AAAAAAAAPMg/d84ZzNgZ-e0m9w1yqOU9bOSekeGtK-4CgCLcBGAs/s640/Most%2BAwesome%2BBlue%2BLake%2BWith%2BSnow%2BMountain.jpeg",
+        "https://3.bp.blogspot.com/-C0ixAfWi-I4/W-5uxBSTtzI/AAAAAAAAPMg/d84ZzNgZ-e0m9w1yqOU9bOSekeGtK-4CgCLcBGAs/s640/Most%2BAwesome%2BBlue%2BLake%2BWith%2BSnow%2BMountain.jpeg",
+        "https://3.bp.blogspot.com/-C0ixAfWi-I4/W-5uxBSTtzI/AAAAAAAAPMg/d84ZzNgZ-e0m9w1yqOU9bOSekeGtK-4CgCLcBGAs/s640/Most%2BAwesome%2BBlue%2BLake%2BWith%2BSnow%2BMountain.jpeg",
+        "https://3.bp.blogspot.com/-C0ixAfWi-I4/W-5uxBSTtzI/AAAAAAAAPMg/d84ZzNgZ-e0m9w1yqOU9bOSekeGtK-4CgCLcBGAs/s640/Most%2BAwesome%2BBlue%2BLake%2BWith%2BSnow%2BMountain.jpeg",
+      ],
+    },
+    date: {
+      fullDate: "1674890763837",
+      year: "2023",
+      month: "01",
+    },
+    status: "published",
+    _id: "63d4ce0b0969cbdfb123b3ff",
+  };
+
+  if (slug == "123") {
+    useEffect(() => {
+      window.document.title = post.title;
+    }, [post.title]);
+    return <Post post={post} author={author} />;
+  } else {
+  }
+};
+export default PostPage;
