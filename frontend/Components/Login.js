486f2c153a1af2b6d55ca7996b2af81e90d24b6c
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-05-10 16:02:35 +0530

    added Login page

---
+++ frontend/Components/Login.js
@@ -0,0 +1,44 @@
+const Login = () => {
+  return (
+    <>
+      <section class="section">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-half">
+              <h1 class="title has-text-centered">Login to admin panel</h1>
+              <form>
+                <div class="field">
+                  <label class="label">Email</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="email"
+                      placeholder="Enter your email"
+                    />
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label">Password</label>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="password"
+                      placeholder="Enter your password"
+                    />
+                  </div>
+                </div>
+                <div class="field">
+                  <div class="control">
+                    <button class="button is-primary">Login</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
---
+++ frontend/pages/admin.js
@@ -0,0 +1,11 @@
+import Login from "../components/Login";
+
+const AdminPage = () => {
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <Login />
+    </div>
+  );
+};
+
+export default AdminPage;
